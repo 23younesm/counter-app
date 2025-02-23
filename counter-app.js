9d8c82dfa63b670e3fdbf90e509c206b3c276f38
23younesm <64438560+23younesm@users.noreply.github.com>
2025-02-23 15:49:35 -0500

    Yippe!

--- counter-app.js
+++ counter-app.js
@@ -51,49 +51,92 @@ export class CounterApp extends DDDSuper(I18NMixin(LitElement)) {
   // Lit scoped styles
   static get styles() {
     return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      :host([count="18"]) {
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        :host([count="18"]) {
         color: var(--ddd-theme-default-original87Pink);
-      }
-      :host([count="21"]) {
+        }
+        :host([count="21"]) {
         color: var(--ddd-theme-default-futureLime);
-      }
-      :host([count="21"]) {
-        color: var(--ddd-theme-default-futureLime);
-      }
-      :host([count=]) {
-        color: var(--ddd-theme-default-futureLime);
-      }
-      
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
-      }
-      .counter {
-        font-size: var(--counter-app-label-font-size, var(--ddd-font-size-xl));
-      }
-    `];
+        }
+        .wrapper.min {
+          color: var(--ddd-theme-default-original87Pink);
+        }
+        .wrapper.max {
+          color: var(--ddd-theme-default-original87Pink);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+        h3 span {
+          font-size: var(--counter-app-label-font-size, var(--ddd-font-size-s));
+        }
+        .counter {
+          font-size: var(--counter-app-label-font-size, var(--ddd-font-size-xl));
+        }
+        .buttons button {
+          margin: 0 5px;
+          padding: 10px;
+          font-size: 16px;
+          cursor: pointer;
+        }
+      `
+    ];
   }
+  
 
   // Lit render the HTML
   render() {
+    const isMin = this.count === this.min;
+    const isMax = this.count === this.max;
     return html`
-    <div class="wrapper">
-    <h1 class="counter">${this.count}</h1>
-    <div class="buttons">
-      <button @click="${this.increase}" ?disabled="${this.count === this.max}">+1</button>
-      <button @click="${this.decrease}" ?disabled="${this.count === this.min}">-1</button>
-    </div>
-    </div>`;
+      <confetti-container id="confetti">
+      <div class="wrapper ${isMin ? 'min' : ''} ${isMax ? 'max' : ''}">
+        <h1 class="counter">${this.count}</h1>
+        <div class="buttons">
+          <button @click="${this.increase}" ?disabled="${this.count === this.max}">+1</button>
+          <button @click="${this.decrease}" ?disabled="${this.count === this.min}">-1</button>
+        </div>
+      </div>
+      </confetti-container>
+    `;
+  }
+  
+  
+updated(changedProperties) {
+  if (super.updated) {
+    super.updated(changedProperties);
+  }
+  if (changedProperties.has("count") && this.count === 21) {
+    // Trigger confetti animation
+    this.makeItRain();
+  }
+  }
+
+  makeItRain() {
+    // this is called a dynamic import. It means it won't import the code for confetti until this method is called
+    // the .then() syntax after is because dynamic imports return a Promise object. Meaning the then() code
+    // will only run AFTER the code is imported and available to us
+    import("@haxtheweb/multiple-choice/lib/confetti-container.js").then(
+      (module) => {
+        // This is a minor timing 'hack'. We know the code library above will import prior to this running
+        // The "set timeout 0" means "wait 1 microtask and run it on the next cycle.
+        // this "hack" ensures the element has had time to process in the DOM so that when we set popped
+        // it's listening for changes so it can react
+        setTimeout(() => {
+          // forcibly set the poppped attribute on something with id confetti
+          // while I've said in general NOT to do this, the confetti container element will reset this
+          // after the animation runs so it's a simple way to generate the effect over and over again
+          this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+        }, 0);
+      }
+    );
   }
 
   increase() {
